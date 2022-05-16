f819edbffc9ef1895b390079a076d1394e061bed
homelith <homemoto.toru@gmail.com>
2022-05-17 00:30:49 +0900

    added env variable workaround for vivado

--- 2020.1/docker/settings.sh
+++ 2020.1/docker/settings.sh
@@ -3,5 +3,6 @@ XILINX_VERSION=2020.1
 
 source /opt/Xilinx/Vitis/${XILINX_VERSION}/settings64.sh
 source /opt/Xilinx/PetaLinux/${XILINX_VERSION}/settings.sh
+export LC_ALL="C"
 echo "---- xilinx-tools:${XILINX_VERSION} container shell ----"
 echo "'$ /root/install.sh' to install tools on /opt"
