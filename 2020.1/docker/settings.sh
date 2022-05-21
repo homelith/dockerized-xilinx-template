657cff6892d957307db0b33f86afe1770d5b6051
homelith <homemoto.toru@gmail.com>
2022-05-21 14:45:31 +0900

    supported pulling docker image from container registry

--- 2020.1/docker/settings.sh
+++ 2020.1/docker/settings.sh
@@ -4,5 +4,11 @@ XILINX_VERSION=2020.1
 source /opt/Xilinx/Vitis/${XILINX_VERSION}/settings64.sh
 source /opt/Xilinx/PetaLinux/${XILINX_VERSION}/settings.sh
 export LC_ALL="C"
+export QT_X11_NO_MITSHM=1
 echo "---- xilinx-tools:${XILINX_VERSION} container shell ----"
-echo "'$ /root/install.sh' to install tools on /opt"
+if !(type vivado > /dev/null 2>&1); then
+	echo "command 'vivado' not found. try '$ /root/install.sh' to install tools on /opt"
+else
+	echo "'vivado' and some xilinx tools successfully introduced on your PATH"
+fi
+xhost +
